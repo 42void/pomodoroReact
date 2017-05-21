93e7d24fd224d85a63dc7698ca88f54fd07843ee
42void <al.vanhoegaerden@gmail.com>
2017-05-21 18:55:27 +0200

    style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,48 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          {/* <h1 id="title">Pomodoro Clock</h1> */}
+
+          <img src={'https://image.flaticon.com/icons/png/128/123/123223.png'} className="App-logo" alt="logo" />
         </div>
-        <p className="App-intro">
+        {/* <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        <div id="setters">
+          <div className="setter">
+            <div className="setterTitle">BREAK LENGTH</div>
+            <div className="plusOrMinus">
+              <button className="item" id="breakMinus">-</button>
+              <div className="item" id="breakLength"></div>
+              <button className="item" id="breakPlus">+</button>
+            </div>
+          </div>
+
+          <div className="setter">
+            <div className="setterTitle">SESSION LENGTH</div>
+            <div className="plusOrMinus">
+              <button className="item" id="sessionMinus">-</button>
+              <div className="item" id="sessionLength"></div>
+              <button className="item" id="sessionPlus">+</button>
+            </div>
+          </div>
+        </div>
+
+        <div id="clock">
+          <div id="circle">
+            <div id="session">
+               SESSION
+            </div>
+            <div id="countDown"></div>
+          </div>
+        </div>
+
+        <div id="resetButtonContainer">
+          <div id="resetButton">
+            RESET
+          </div>
+        </div>
+
       </div>
     );
   }
