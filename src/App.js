b2ca1c05267fb1c38bd30c83d43e1d58ca91ab18
42void <al.vanhoegaerden@gmail.com>
2017-05-21 20:18:01 +0200

    set state and timer  according to button onClick

--- src/App.js
+++ src/App.js
@@ -2,7 +2,60 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      timer:25,
+      breaak:5,
+      session:25,
+      isSessionNow: true,
+      isPausedNow: false
+    };
+    // Toggle the state every second
+    // setInterval(() => {
+    //   this.setState({ showText: !this.state.showText });
+    // }, 1000);
+  }
+
   render() {
+
+    const plusSession = () => {
+      var newSession = this.state.session + 1;
+      if(this.state.isSessionNow){
+        this.setState({session: newSession, timer: this.state.session})
+      }else{
+        this.setState({session: newSession})
+      }
+    }
+
+    const minusSession = () => {
+      var newSession = this.state.session - 1;
+      if(this.state.isSessionNow){
+        this.setState({session: newSession, timer: this.state.session})
+      }else{
+        this.setState({session: newSession})
+      }
+    }
+
+    const plusBreak = () => {
+      var newBreak = this.state.breaak + 1;
+      if(this.state.isSessionNow){
+        this.setState({breaak: newBreak})
+      }else{
+        this.setState({breaak: newBreak, timer: this.state.newBreak})
+      }
+    }
+
+    const minusBreak = () => {
+      var newBreak = this.state.breaak - 1;
+      if(this.state.isSessionNow){
+        this.setState({breaak: newBreak})
+      }else{
+        this.setState({breaak: newBreak, timer:this.state.newBreak})
+      }
+    }
+
     return (
       <div className="App">
         <div className="App-header">
@@ -14,18 +67,18 @@ class App extends Component {
           <div className="setter">
             <div className="setterTitle">BREAK LENGTH</div>
             <div className="plusOrMinus">
-              <button className="item" id="breakMinus">-</button>
-              <div className="item" id="breakLength"></div>
-              <button className="item" id="breakPlus">+</button>
+              <button className="item" id="breakMinus" onClick={minusBreak}>-</button>
+              <div className="item" id="breakLength">{this.state.breaak}</div>
+              <button className="item" id="breakPlus" onClick={plusBreak}>+</button>
             </div>
           </div>
 
           <div className="setter">
             <div className="setterTitle">SESSION LENGTH</div>
             <div className="plusOrMinus">
-              <button className="item" id="sessionMinus">-</button>
-              <div className="item" id="sessionLength"></div>
-              <button className="item" id="sessionPlus">+</button>
+              <button className="item" id="sessionMinus" onClick={minusSession}>-</button>
+              <div className="item" id="sessionLength">{this.state.session}</div>
+              <button className="item" id="sessionPlus" onClick={plusSession}>+</button>
             </div>
           </div>
         </div>
@@ -35,7 +88,7 @@ class App extends Component {
             <div id="session">
                SESSION
             </div>
-            <div id="countDown"></div>
+            <div id="countDown">{this.state.timer}</div>
           </div>
         </div>
 
